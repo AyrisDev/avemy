ec141cd9b009a3e4b91e61a09bdaf5c011f2ed5f
mstfyldz <mstfyldz@mstfyldz-MacBook-Air.local>
2026-01-23 02:07:31 +0300

    b

--- lib/directus.ts
+++ lib/directus.ts
@@ -3,10 +3,10 @@ import { createDirectus, rest, staticToken } from '@directus/sdk';
 const DIRECTUS_URL = process.env.NEXT_PUBLIC_DIRECTUS_URL || 'http://localhost:8055';
 const DIRECTUS_TOKEN = process.env.DIRECTUS_TOKEN;
 
-const client = createDirectus(DIRECTUS_URL).with(rest());
+let client = createDirectus(DIRECTUS_URL).with(rest());
 
 if (DIRECTUS_TOKEN) {
-    client.with(staticToken(DIRECTUS_TOKEN));
+    client = client.with(staticToken(DIRECTUS_TOKEN));
 }
 
 // Function to get image URL
